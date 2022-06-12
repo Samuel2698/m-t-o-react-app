d41c20a7b5c0956116d80cf5e72ce884ea9ad308
Samuel2698 <97331279+Samuel2698@users.noreply.github.com>
2022-06-12 16:38:36 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import axios from 'axios'
+
+function App() {
+
+  //const url = `https://api.openweathermap.org/data/2.5/weather?q=paris&appid=4a81816180e2bc38c902b0829da85882`
+
+  return (
+    <div className="app">
+      Hola
+    </div>
+  );
+}
+
+export default App;
