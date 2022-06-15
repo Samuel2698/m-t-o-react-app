d7f3ce9f4549d6169f73adb33e7af7959b29d7d6
Samuel Velasco <samuelvelasco2698@gmail.com>
2022-06-15 15:13:25 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {useState} from "react";
 import axios from 'axios'
 
+
 function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=4a81816180e2bc38c902b0829da85882`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=4a81816180e2bc38c902b0829da85882`
 
   const searchLocation = (event) => {
     if(event.key === 'Enter') {
@@ -23,7 +24,7 @@ function App() {
         <input
           value={location}
           type="text"
-          placeholder="Emplacement:"
+          placeholder="Enter location"
           onChange={event => setLocation(event.target.value)}
           onKeyPress={searchLocation}
         />
@@ -34,29 +35,35 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            {data.main ? <h1>{data.main.temp}</h1> : null}
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
           </div>
           <div className="description">
-             <p>Ensoleillé</p>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
+
+      {data.name != undefined && 
         <div className="bottom">
           <div className="feels">
-            <p className="bold">28°C</p>
-            <p>Ressenti</p>
+            {data.main ? <p className="bold">{data.main.feels_like.toFixed()}°C</p> : null}
+            <p>Feels L.</p>
           </div>
           <div className="humidity">
-            <p className="bold">32%</p>
-            <p>Humidité</p>
+            {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+            <p>Humidity</p>
           </div>
           <div className="wind">
-            <p className="bold">16 km/h</p>
-            <p>Vent</p>
+            {data.wind ? <p className="bold">{data.wind.speed.toFixed()} MPH</p> : null}
+            <p>Wind</p>
           </div>
         </div>
+      }
+
+
       </div>
     </div>
   );
 }
 
 export default App;
+
